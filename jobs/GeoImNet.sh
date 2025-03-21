cfc588e4844d885a6e4b7ce2cc854ea41159ae4f
Soh Yoshida <meruemon@gmail.com>
2025-03-21 14:44:24 +0900

    Update GeoImNet.sh

--- jobs/GeoImNet.sh
+++ jobs/GeoImNet.sh
@@ -7,14 +7,12 @@ lambda_b=$4
 lambda_c=$5
 mode=$6
 
-
 dataset="GeoImNet"
 data_dir="./data/GeoImNet"
 out_dir=MemSAC_${source}${target}
 nClasses=600
 BatchSize=32
 queue_size=72000
-
 
 python3 train.py --dataset ${dataset} --source ${source} --target ${target} --lr 0.03 --out_dir ${out_dir} --max_iteration 100000 \
 --batch_size ${BatchSize} --data_dir ${data_dir} --total_classes ${nClasses} --multi_gpu 0 --test-iter 5000 --queue_size ${queue_size} \
